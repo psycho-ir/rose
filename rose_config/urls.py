00fa71e278a5e249157ecd0a55bc083e68e62080
soroosh <soroosh.sarabadani@gmail.com>
2014-04-27 11:22:29 +0430

    Loan Type connected to refund type

--- rose_config/urls.py
+++ rose_config/urls.py
@@ -6,4 +6,6 @@ from rose_config.views import *
 urlpatterns = patterns('',
                        url(r'^(?i)town/province_id=(?P<province_id>\d*)',
                            login_required(TownView.as_view(), login_url='/'), name='town'),
+                       url(r'^(?i)refund/loan_type_id=(?P<loan_type_id>\d*)',
+                           login_required(RefundTypeView.as_view(), login_url='/'), name='refund_type'),
 )
--- rose_config/views.py
+++ rose_config/views.py
@@ -2,11 +2,18 @@ from django.core import serializers
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
-from rose_config.models import Town
+from rose_config.models import Town, LoanType, RefundType
 
 
 class TownView(View):
     def get(self, request, province_id):
         towns = Town.objects.filter(province_id=province_id)
         encoded = serializers.serialize('json', towns)
+        return HttpResponse(encoded)
+
+
+class RefundTypeView(View):
+    def get(self, request, loan_type_id):
+        refund_types = LoanType.objects.get(id=loan_type_id).refund_types.all()
+        encoded = serializers.serialize('json', refund_types)
         return HttpResponse(encoded)
