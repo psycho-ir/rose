a3cb857531937797d44901fa386f5636f0ce20a2
soroosh <soroosh.sarabadani@gmail.com>
2014-04-30 11:15:04 +0430

    guarantor url added

--- guarantor/urls.py
+++ guarantor/urls.py
@@ -1,1 +1,9 @@
 __author__ = 'soroosh'
+
+from django.conf.urls import patterns, url
+
+from guarantor.views import *
+
+urlpatterns = patterns('',
+                       url(r'^(?i)register/(?P<request_id>\d*)/(?P<customer_id>\d*)', RegisterGuarantorView.as_view(), name='register'),
+)
--- rose/urls.py
+++ rose/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
                        url(r'^(?i)core/', include('core_connection.urls', namespace='core')),
                        url(r'^(?i)customer/', include('customer.urls', namespace='customer')),
                        url(r'^(?i)config/', include('rose_config.urls', namespace='config')),
+                       url(r'^(?i)guarantor/', include('guarantor.urls', namespace='guarantor')),
                        url(r'^$', LoginView.as_view()),
 )
 
