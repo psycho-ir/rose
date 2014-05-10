be6967592468830ee1b7ed88783d7dc6ba93d764
soroosh <soroosh.sarabadani@gmail.com>
2014-05-10 11:15:13 +0430

    company type init data created

--- init_data.sh
+++ init_data.sh
@@ -1,1 +1,1 @@
-python2.7 manage.py loaddata business_part.json request_description.json job_type.json loan_type.json refund_type.json initial_data.json province.json town.json business_place.json vasighe_type.json bank.json
+python2.7 manage.py loaddata business_part.json request_description.json job_type.json loan_type.json refund_type.json initial_data.json province.json town.json business_place.json vasighe_type.json bank.json company_type.json
