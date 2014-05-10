56e40209b78cacf99d75f2d59362c30e4f1d705c
soroosh <soroosh.sarabadani@gmail.com>
2014-05-10 14:24:25 +0430

    date problems resolved

--- customer/models/enterprise_models.py
+++ customer/models/enterprise_models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from jdatetime import datetime as jalali_datetime
 from customer.models import Customer
 from customer.models.real_models import RealCustomerInformation
 from rose_config.models import CompanyType, Province, Town, JobCertificateType
+from utils.date_utils import greg_date_from_shamsi
 
 __author__ = 'soroosh'
 
@@ -9,6 +11,7 @@ __author__ = 'soroosh'
 class BoadOfDirectorRole(models.Model):
     class Meta:
         app_label = 'customer'
+
     name = models.CharField(max_length=50)
     local_name = models.CharField(max_length=50)
     description = models.CharField(max_length=60, default="")
@@ -18,6 +21,7 @@ class BoadOfDirectorRole(models.Model):
 class RegisterTown(models.Model):
     class Meta:
         app_label = 'customer'
+
     name = models.CharField(max_length=50)
     local_name = models.CharField(max_length=50)
     description = models.CharField(max_length=60, default="")
@@ -27,23 +31,36 @@ class RegisterTown(models.Model):
 class EnterpriseCustomerInformation(Customer):
     class Meta:
         app_label = 'customer'
+
     company_name = models.CharField(max_length=256)
     register_number = models.IntegerField()
     company_type = models.ForeignKey(CompanyType)
-    register_date = models.DateField()
+    register_date = models.DateTimeField()
     newspaper_number = models.IntegerField()
-    newspaper_date = models.DateField()
+    newspaper_date = models.DateTimeField()
     register_place = models.ForeignKey(RegisterTown)
+
+    def get_persian_register_date(self):
+        return jalali_datetime.fromgregorian(datetime=self.register_date).strftime('%Y/%m/%d')
+
+    def get_persian_newspaper_date(self):
+        return jalali_datetime.fromgregorian(datetime=self.newspaper_date).strftime('%Y/%m/%d')
 
     @staticmethod
     def from_dic(dic):
-        c = EnterpriseCustomerInformation(company_name=dic['company_name'],
-                                          register_number=dic['register_number'],
-                                          company_type=dic['company_type'],
-                                          register_date=dic['register_date'],
-                                          newspaper_number=dic['newspaper_number'],
-                                          newspaper_date=dic['newspaper_date'],
-                                          register_place=dic['register_place']
+        print dic
+        register_date = greg_date_from_shamsi(dic['register_date'], '/')
+        print register_date
+        newspaper_date = greg_date_from_shamsi(dic['newspaper_date'], '/')
+        c = EnterpriseCustomerInformation(
+            customer_code=dic['company_id'],
+            company_name=dic['company_name'],
+            register_number=dic['register_number'],
+            company_type_id=dic['company_type_id'],
+            register_date=register_date,
+            newspaper_number=dic['newspaper_number'],
+            newspaper_date=newspaper_date,
+            register_place_id=dic['register_place_id']
         )
 
         return c
@@ -52,6 +69,7 @@ class EnterpriseCustomerInformation(Customer):
 class BoardOfDirector(models.Model):
     class Meta:
         app_label = 'customer'
+
     company = models.ForeignKey(EnterpriseCustomerInformation)
     customer = models.ForeignKey(RealCustomerInformation)
     role = models.ForeignKey(BoadOfDirectorRole)
@@ -75,7 +93,8 @@ class BoardOfDirector(models.Model):
 class EnterpriseContactInformation(models.Model):
     class Meta:
         app_label = 'customer'
-    company = models.OneToOneField(EnterpriseCustomerInformation, primary_key=True)
+
+    company = models.OneToOneField(EnterpriseCustomerInformation, related_name='contact_info', primary_key=True)
     province = models.ForeignKey(Province)
     town = models.ForeignKey(Town)
     address = models.CharField(max_length=256)
@@ -99,20 +118,31 @@ class EnterpriseContactInformation(models.Model):
 class EnterpriseActivity(models.Model):
     class Meta:
         app_label = 'customer'
+
+    company = models.OneToOneField(EnterpriseCustomerInformation, related_name='activity_info', primary_key=True)
     activity_type = models.CharField(max_length=256)
     certificate_type = models.ForeignKey(JobCertificateType)
     certificate_number = models.CharField(max_length=20)
-    certificate_start_date = models.DateField()
-    certificate_expire_date = models.DateField()
+    certificate_start_date = models.DateTimeField()
+    certificate_expire_date = models.DateTimeField()
+
+    def get_persian_certificate_start_date(self):
+        return jalali_datetime.fromgregorian(datetime=self.certificate_start_date).strftime('%Y/%m/%d')
+
+    def get_persian_certificate_expire_date(self):
+        return jalali_datetime.fromgregorian(datetime=self.certificate_expire_date).strftime('%Y/%m/%d')
 
     @staticmethod
     def from_dic(dic):
+        certificate_start_date = greg_date_from_shamsi(dic['certificate_start_date'], '/')
+        certificate_expire_date = greg_date_from_shamsi(dic['certificate_expire_date'], '/')
         ea = EnterpriseActivity(
+            company_id=dic['company_id'],
             activity_type=dic['activity_type'],
-            certificate_type=dic['certificate_type'],
+            certificate_type_id=dic['certificate_type_id'],
             certificate_number=dic['certificate_number'],
-            certificate_start_date=dic['certificate_start_date'],
-            certificate_expire_date=dic['certificate_expire_date']
+            certificate_start_date=certificate_start_date,
+            certificate_expire_date=certificate_expire_date
         )
 
         return ea
