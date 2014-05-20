0f454f21536dcdb9890919eb4a467b9d5dd3bb7a
soroosh <soroosh.sarabadani@gmail.com>
2014-05-20 17:29:27 +0430

    notification context processor added

---
+++ notification/context_processor.py
@@ -0,0 +1,8 @@
+from superior.models import Assign
+
+__author__ = 'soroosh'
+
+
+def notification(request):
+    assigns = Assign.objects.filter(target__id=request.user.id)
+    return {'notifications': assigns}
