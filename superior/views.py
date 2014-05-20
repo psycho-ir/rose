313f2c48584fc52adde5c1ed1d54ff839dbacc0e
soroosh <soroosh.sarabadani@gmail.com>
2014-05-20 17:30:11 +0430

    post method create

--- superior/views.py
+++ superior/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.db.models import Q
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader, RequestContext
 from django.views.generic import View
 from start_grant.models import Request
-from superior.models import EnquiryAction
+from superior.models import EnquiryAction, EnquiryAssign
+from utils.date_utils import greg_date_from_shamsi
 
 
 class TaskListView(View):
@@ -12,7 +15,7 @@ class TaskListView(View):
         if request.user.profile.role.name != 'superior':
             return HttpResponse('No Access')
         template = loader.get_template('tasks_list.html')
-        requests = Request.objects.filter(status='checklist_completed', branch_code=request.user.profile.branch_code)
+        requests = Request.objects.filter(Q(status='checklist_completed') | Q(status='enquiry_assigned'), branch_code=request.user.profile.branch_code)
         context = RequestContext(request, {'requests': requests})
         return HttpResponse(template.render(context))
 
@@ -29,6 +32,29 @@ class EnquiryAssignView(View):
                                                      profile__role__name='teller')
 
         template = loader.get_template('enquiry_assign.html')
-        context = RequestContext(request, {'actions': selectable_actions, 'target_users': candidate_target_users})
+        context = RequestContext(request, {'request_id': request_id, 'actions': selectable_actions,
+                                           'target_users': candidate_target_users})
 
         return HttpResponse(template.render(context))
+
+    def post(self, request, request_id):
+        if request.user.profile.role.name != 'superior':
+            return HttpResponse('No Access')
+
+        customer_request = Request.objects.get(id=request_id)
+        expire_date = greg_date_from_shamsi(request.POST['expire_date'], '/')
+        assign = EnquiryAssign()
+        assign.source_id = request.user.id
+        assign.target_id = request.POST['target_user']
+        assign.comment = request.POST['comment']
+        assign.priority = request.POST['priority']
+        assign.expire_date = expire_date
+        assign.request_id = request_id
+        assign.save()
+        for item in request.POST.getlist('selected_actions'):
+            assign.actions.add(int(item))
+
+        customer_request.status = 'enquiry_assigned'
+        customer_request.save()
+
+        return HttpResponseRedirect(reverse('superior:tasks'))
