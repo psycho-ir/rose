52046de1c8a50c0d4bb96c09494391887add6e87
soroosh <soroosh.sarabadani@gmail.com>
2014-05-24 15:02:31 +0430

    urls added

--- assign/urls.py
+++ assign/urls.py
@@ -6,8 +6,15 @@ from assign.views import *
 
 
 urlpatterns = patterns('',
-                       url(r'^(?i)enquiry_assign/(?P<request_id>\d*)', login_required(EnquiryAssignView.as_view(), login_url='/'),
-                           name='enquiry_assign')
+                       url(r'^(?i)enquiry_assign/(?P<request_id>\d*)',
+                           login_required(EnquiryAssignView.as_view(), login_url='/'),
+                           name='enquiry_assign'),
+                       url(r'^(?i)enquiry_response/(?P<assign_id>\d*)',
+                           login_required(EnquiryResponseView.as_view(), login_url='/'), name='enquiry_response'),
+                       url(r'^(?i)enquiry_response_start',
+                           login_required(EnquiryActionResponseStartView.as_view(), login_url='/'),
+                           name='enquiry_response_start'),
+
 
 )
 
