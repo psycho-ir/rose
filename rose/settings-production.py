3155250fd0cbf3aeb236bfde509d39018777cf16
soroosh <soroosh.sarabadani@gmail.com>
2014-06-03 14:52:32 +0430

    production created

---
+++ rose/settings-production.py
@@ -0,0 +1,150 @@
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+COMPRESS_ROOT = os.path.join(BASE_DIR, 'stat')
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'o**w8ex&cydi*(r26ajcc71^k8kb099)4(36zjr9i&q4v7rex@'
+
+INTERNAL_IPS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"),)
+
+# Application definition
+INSTALLED_APPS = (
+    'customer',
+    'dashboard',
+    'user_profile',
+    'django.contrib.auth',
+    'user_management',
+    'core_connection',
+    'rose_config',
+    'start_grant',
+    'guarantor',
+    'assign',
+    'superior',
+    'notification',
+    'south',
+    'compressor',
+    'django_scheduler',
+    'django.contrib.admin',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles'
+
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    # 'django.core.context_processors.request',
+    'django.contrib.messages.context_processors.messages',
+    'notification.context_processor.notification'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.gzip.GZipMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+)
+
+ROOT_URLCONF = 'rose.urls'
+
+WSGI_APPLICATION = 'rose.wsgi.application'
+from django.db import connections
+from django.db import connection
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'orcl',
+        'USER': 'rose',
+        'PASSWORD': 'rose',
+        'HOST': 'dbserver',
+        'PORT': '1521',
+    },
+
+    'core': {
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'orcl',
+        'USER': 's5samens',
+        'PASSWORD': 's5samens',
+        'HOST': 'dbserver',
+        'PORT': '1521',
+        'OPTIONS': {
+            'threaded': True,
+            'use_returning_into': False,
+        },
+    }
+}
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'fa-ir'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, "locale"),  # Assuming BASE_DIR is where your manage.py file is
+)
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+DEFAULT_LOGIN_URL = '/grant/track'
+DEFAULT_LOGOUT_URL = '/'
+
+# STATIC resources config
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'stat')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # other finders..
+    'compressor.finders.CompressorFinder',
+)
+
+
+# #### CACHE config ##########
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
---
+++ rose/wsgi-production.py
@@ -0,0 +1,29 @@
+"""
+WSGI config for rose project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rose.settings-production")
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
+
+# import datetime
+#
+# from django_scheduler.simple_scheduler import ProcessSimpleScheduler
+# from assign.models import *
+#
+#
+# def say_hello():
+#     print 'Number of enquiry assigns: %s' % EnquiryAssign.objects.count()
+#
+#
+# scheduler = ProcessSimpleScheduler(5, say_hello)
+# scheduler.run()
